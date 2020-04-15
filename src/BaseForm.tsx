82ca39a31d6d95c207f8fce626ec5c517dbb3628
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-15 10:05:22 +0100

    feat: add forms

---
+++ src/BaseForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BaseForm } from 'uniforms';
+
+function Ionic(parent: any): any {
+  class _ extends parent {
+    static Ionic = Ionic;
+
+    static displayName = `Ionic${parent.displayName}`;
+  }
+
+  return _;
+}
+
+export default Ionic(BaseForm);
