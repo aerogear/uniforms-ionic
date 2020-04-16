a5d4d9a0208000e9df5889835203b426b899f322
Gianluca <gianlucazuccarelli@gmail.com>
2020-04-16 17:15:19 +0100

    fix: example app

--- examples/src/index.js
+++ examples/src/index.js
@@ -22,9 +22,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
